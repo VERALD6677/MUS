b275a3e5c7b80bbbcd80d6c9004b23f08b1d8d68
VERALD6677 <44982435+VERALD6677@users.noreply.github.com>
2024-04-17 20:31:41 +0300

    Update Mus.sql

--- Mus.sql
+++ Mus.sql
@@ -1,46 +1,61 @@
-CREATE TABLE IF NOT EXISTS Musical_genres (
-    id SERIAL PRIMARY KEY,
-    NameMusgen VARCHAR(80) NOT NULL
+CREATE TABLE `Musical_genres` (
+  `id` SERIAL PRIMARY KEY AUTO_INCREMENT,
+  `NameMusgen` VARCHAR(80) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Executors (
-    id SERIAL PRIMARY KEY,
-    NameExecut VARCHAR(80) NOT NULL
+CREATE TABLE `Executors` (
+  `id` SERIAL PRIMARY KEY AUTO_INCREMENT,
+  `NameExecut` VARCHAR(80) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Musgen_Execut (
-    musgen_id INTEGER REFERENCES Musical_genres(id),
-    execut_id INTEGER REFERENCES Executors(id),
-    CONSTRAINT ME PRIMARY KEY (musgen_id, execut_id)
+CREATE TABLE `Musgen_Execut` (
+  `musgen_id` INTEGER,
+  `execut_id` INTEGER,
+  PRIMARY KEY (`musgen_id`, `execut_id`)
 );
 
-CREATE TABLE IF NOT EXISTS Albums (
-    id SERIAL PRIMARY KEY,
-    NameAlbum VARCHAR(80) NOT NULL,
-    YearAlbum INTEGER
+CREATE TABLE `Albums` (
+  `id` SERIAL PRIMARY KEY AUTO_INCREMENT,
+  `NameAlbum` VARCHAR(80) NOT NULL,
+  `YearAlbum` INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS Album_Executors (
-    album_id INTEGER REFERENCES Albums(id),
-    execut_id INTEGER REFERENCES Executors(id),
-    CONSTRAINT AE PRIMARY KEY (album_id, execut_id)
+CREATE TABLE `Album_Executors` (
+  `album_id` INTEGER,
+  `execut_id` INTEGER,
+  PRIMARY KEY (`album_id`, `execut_id`)
 );
 
-CREATE TABLE IF NOT EXISTS Tracks (
-    id SERIAL PRIMARY KEY,
-    album_id INTEGER REFERENCES Albums(id),
-    NameTrack VARCHAR(40) NOT NULL,
-    Duration INTEGER
+CREATE TABLE `Tracks` (
+  `id` SERIAL PRIMARY KEY AUTO_INCREMENT,
+  `album_id` INTEGER,
+  `NameTrack` VARCHAR(40) NOT NULL,
+  `Duration` INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS Collections (
-    id SERIAL PRIMARY KEY,
-    NameCollect VARCHAR(40) NOT NULL,
-    YearCollect INTEGER
+CREATE TABLE `Collections` (
+  `id` SERIAL PRIMARY KEY AUTO_INCREMENT,
+  `NameCollect` VARCHAR(40) NOT NULL,
+  `YearCollect` INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS Track_Collections (
-    track_id INTEGER REFERENCES Tracks(id),
-    collect_id INTEGER REFERENCES Collections(id),
-    CONSTRAINT TC PRIMARY KEY (track_id, collect_id)
+CREATE TABLE `Track_Collections` (
+  `track_id` INTEGER,
+  `collect_id` INTEGER,
+  PRIMARY KEY (`track_id`, `collect_id`)
 );
+
+ALTER TABLE `Musgen_Execut` ADD FOREIGN KEY (`musgen_id`) REFERENCES `Musical_genres` (`id`);
+
+ALTER TABLE `Musgen_Execut` ADD FOREIGN KEY (`execut_id`) REFERENCES `Executors` (`id`);
+
+ALTER TABLE `Album_Executors` ADD FOREIGN KEY (`album_id`) REFERENCES `Albums` (`id`);
+
+ALTER TABLE `Album_Executors` ADD FOREIGN KEY (`execut_id`) REFERENCES `Executors` (`id`);
+
+ALTER TABLE `Tracks` ADD FOREIGN KEY (`album_id`) REFERENCES `Albums` (`id`);
+
+ALTER TABLE `Track_Collections` ADD FOREIGN KEY (`track_id`) REFERENCES `Tracks` (`id`);
+
+ALTER TABLE `Track_Collections` ADD FOREIGN KEY (`collect_id`) REFERENCES `Collections` (`id`);
+
